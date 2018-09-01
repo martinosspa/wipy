0ec6f49cb8821460a6fef140c7996072c3719d5c
martinosspa <martinosspa@gmail.com>
2018-09-01 20:58:57 +0300

    updated wiAPI

--- wiAPI.py
+++ wiAPI.py
@@ -5,11 +5,19 @@ import time
 import numpy as np
 warframe_market_api_base_url = 'https://api.warframe.market/v1/items'
 warframe_drop_data_url = 'http://drops.warframestat.us/data'
-ERA_LITH = 'lith'
-ERA_MESO = 'meso'
-ERA_NEO = 'neo'
-ERA_AXI = 'axi'
-FORMA = 'forma'
+ERA_LITH = 'Lith'
+ERA_MESO = 'Meso'
+ERA_NEO = 'Neo'
+ERA_AXI = 'Axi'
+FORMA = 'Forma'
+MISSION_ROTATION_A = 'A'
+MISSION_ROTATION_B = 'B'
+MISSION_ROTATION_C = 'C'
+MISSION_ROTATIONS = [MISSION_ROTATION_A, MISSION_ROTATION_B, MISSION_ROTATION_C]
+RELIC_TIER0 = 0
+RELIC_TIER1 = 1
+RELIC_TIER2 = 2
+RELIC_TIER3 = 3
 ERAS = [ERA_LITH, ERA_MESO, ERA_NEO, ERA_AXI]
 ORDER_SELL = 'sell'
 ORDER_BUY = 'buy'
@@ -36,6 +44,7 @@ def _filter(order):
 	order['user'].pop('reputation')
 	order['user'].pop('reputation_bonus')
 	return order
+
 def get_all_items():
 	req = Request(warframe_market_api_base_url)
 	data = json.loads(urlopen(req).read())['payload']['items']['en']
@@ -59,16 +68,20 @@ class Reward:
 		self.price = None
 
 class Forma(Reward):
-	def __init__(self):
+	def __init__(self, drop_chance):
 		Reward.__init__(self, FORMA)
 		self.name = FORMA
-		self.price = 10
+		self.drop_chance = drop_chance
+		self.price = None
 		self.customer = None
 
+	def get_price(self, _):
+		self.price = 10
+
 class PrimePart(Reward):
-	def __init__(self, part_name):
+	def __init__(self, part_name, drop_chance):
 		Reward.__init__(self, part_name)
-		
+		self.drop_chance = drop_chance
 		drop_rates = [[]]
 		self.name = decode(part_name)
 		self.url_name = encode(part_name)
@@ -83,17 +96,16 @@ class PrimePart(Reward):
 		self.orders = json.loads(urlopen(self.req).read())['payload']['orders']
 		self.prices, self.customer_info = [], []
 
-		#filter orders
+		#filters orders
 		self.orders = list(map(_filter, self.orders))
 		self.orders = list(filter(lambda order: order['order_type'] == order_type.lower(), self.orders))
 		self.orders = list(filter(lambda order: order['platform'] == 'pc', self.orders))
 		self.orders = list(filter(lambda order: order['user']['status'] == 'ingame', self.orders))
+
 		self.prices = list(map(lambda order: order['platinum'], self.orders))
-		self.customer_info = list(map(lambda order: {order['user']['ingame_name'], 
-													 order['user']['region']}, self.orders ))
-		#print(self.orders)
+		self.customer_info = list(map(lambda order: {'username' : order['user']['ingame_name'], 
+													 'region': order['user']['region']}, self.orders ))
 		self.position = np.argmin(self.prices) if order_type == ORDER_SELL else np.argmax(self.prices)
-		self.position = 0
 		self.price = self.prices[self.position]
 		self.customer = self.customer_info[self.position]
 		self.end_time = time.time()
@@ -103,33 +115,66 @@ class PrimePart(Reward):
 
 
 class Relic:
-	def __init__(self, relic_era, relic_name, verbose=False):
+	def __init__(self, relic_era, relic_name, drop_chance, verbose=False):
 		if not relic_era in ERAS:
 			raise ValueError('not a valid relic era')
 		self.era = relic_era
 		self.name = encode(relic_name)
 		self.url_name = decode(relic_name)
+		self.rewards = None
+		self.drop_chance = drop_chance
+		self.total_price = 0
 	def load_rewards(self, verbose=False):
 		self.start_time = time.time()
 		self.raw_data = get_drop_data('/relics/{}/{}.json'.format(self.era.capitalize(),
 																  self.url_name.capitalize()),
 																  verbose=verbose)
-
 		self.rewards = []
 		for reward in self.raw_data['rewards']['Intact']:
 			self.split_name = reward['itemName'].lower().split(' ')
 			if 'forma' in self.split_name:
-				self.rewards.append(Forma())
+				self.rewards.append(Forma(reward['chance']))
 			else:
-				self.rewards.append(PrimePart(reward['itemName']))
+				self.rewards.append(PrimePart(reward['itemName'], reward['chance']))
 		self.end_time = time.time()
 		if verbose:
 			print('{} took {} seconds'.format(self.url_name, self.end_time - self.start_time))
 
-r = Relic(ERA_AXI, 'r1')
-r.load_rewards(verbose=False)
-#print(r.rewards)
-for reward in r.rewards:
-	reward.get_price(ORDER_SELL)
-	#print(reward.price)
-	
+	def get_sell_price(self, relic_tier):
+		if relic_tier not in range(0, 4):
+			raise ValueError('not a valid relic tier')
+		for reward in self.rewards:
+			reward.get_price(ORDER_SELL)
+			self.total_price += reward.drop_chance / 100 * reward.price
+class Mission:
+	def __init__(self, mission_planet, mission_name):
+		self.planet = mission_planet
+		self.name = mission_name
+		self.rewards = []
+	def load_rewards(self, rotation):
+		if not rotation in MISSION_ROTATIONS:
+			raise ValueError('not a valid mission rotation')
+		self.raw_data = get_drop_data('/missionRewards/{}/{}.json'.format(self.planet.capitalize(),
+																		 self.name.capitalize()))['rewards'][rotation]
+		self.raw_data = list(filter(lambda relic: 'Relic' in relic['itemName'].split(' '), self.raw_data))
+		for relic in self.raw_data: 
+			self.split_name = relic['itemName'].split(' ')
+			self.rewards.append(Relic(self.split_name[0], self.split_name[1], relic['chance']))
+
+	def get_reward_total_sell_price(self, rotation=MISSION_ROTATION_A, relic_tier=RELIC_TIER0):
+		self.load_rewards(rotation)
+		self.prices = []
+		self.total_sell_price = 0
+		for relic in self.rewards:
+			relic.load_rewards() 
+			relic.get_sell_price(relic_tier)
+			#print(relic.drop_chance)
+			self.prices.append({'era':relic.era,
+							   'name': relic.name,
+							   'total_price':relic.total_price})
+			self.total_sell_price += relic.drop_chance / 100 * relic.total_price
+		return self.total_sell_price
+
+
+m = Mission('sedna', 'hydron')
+print(m.get_reward_total_sell_price(rotation=MISSION_ROTATION_C, relic_tier=RELIC_TIER3))
