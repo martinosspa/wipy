2f0c87f46aacb45eff81e9f568b4afbaa31ae7e6
martinosspa <35999764+martinosspa@users.noreply.github.com>
2019-07-16 13:21:12 +0300

    final all_missions commit

--- all_missions.py
+++ all_missions.py
@@ -10,31 +10,45 @@ _mission_endpoint = 'data/missionRewards.json'
 interested_game_modes = ['Defense', 'Interception', 'Survival']
 not_intrested_rewards = ['Credits', 'Endo']
 interested_items = []
-def _filter_planet_missions(planet):
+
+def _filter_name(name):
+	return name.lower().replace(' ', '_').replace('-', '_').replace('\'', '')
+
+def _filter_planet_missions(planet_name):
 	# filters the missions of a planet if game mode is not desired
-	for mission in dict(planet):
-		if planet[mission]['gameMode'] not in interested_game_modes:
-			planet.pop(mission)
+	for mission in dict(planet_name):
+		if planet_name[mission]['gameMode'] not in interested_game_modes:
+			planet_name.pop(mission)
 
+def add_to_interested_items(planet_name):
+	# adds all items that are not credits and endo to a list
+	for mission in all_planets[planet_name]:
+		for rotation in all_planets[planet_name][mission]['rewards']:
+			for reward in all_planets[planet_name][mission]['rewards'][rotation]:
+				if reward['itemName'] not in interested_items:
+					interested_items.append(reward['itemName'])
 
-
-def process_planet(planet):
+def process_planet(planet_name):
 	# filter unwanted reward	
-	planets_copy = dict(all_planets)
-
-	for mission in all_planets[planet]:
-		for rotation in all_planets[planet][mission]['rewards']:
-			_rotation = all_planets[planet][mission]['rewards'][rotation]
-			rewards = list(filter(lambda r: not_intrested_rewards[0] in r['itemName'], _rotation))
-			rewards = list(filter(lambda r: not_intrested_rewards[1] in r['itemName'], _rotation))
-	#with open(f'all-items-json/{planet}.json', 'w') as f:
-	#	json.dump(all_planets[planet], f)	
+	for mission in all_planets[planet_name]:
+		for rotation in all_planets[planet_name][mission]['rewards']:
+			rewards = all_planets[planet_name][mission]['rewards'][rotation]
+			rewards = list(filter(lambda r: not not_intrested_rewards[0] in r['itemName'], rewards))
+			rewards = list(filter(lambda r: not not_intrested_rewards[1] in r['itemName'], rewards))
+			
+			all_planets[planet_name][mission]['rewards'][rotation] = rewards
 
 
+def dump_planet(planet_name):
+	# dump planet data as a json
+	with open(f'all-items-json/{planet_name}.json', 'w') as f:
+		json.dump(all_planets[planet_name], f)
 
 
 
-async def main():
+
+async def get_items():
+	global interested_items
 	async with aiohttp.ClientSession() as session:
 		resp2 = await session.get(f'{wilib2._drop_url}/{_mission_endpoint}')
 		global all_planets
@@ -46,10 +60,22 @@ async def main():
 			_filter_planet_missions(all_planets[planet])
 
 		with ThreadPool(8) as pool:
-			results = pool.map(process_planet, all_planets)
-			#zip(itertools.repeat(constant), list_a)
-		#with open('test.json', 'w') as f:
-		#	json.dump(all_planets, f)
+			pool.map(process_planet, all_planets)
+			pool.map(add_to_interested_items, all_planets)
+			print(len(interested_items)) 
+
+		relics = []
+		for item in interested_items:
+			if 'Relic' in item:
+				relics.append(item)
+		for r in relics:
+			interested_items.remove(r)
+		interested_items = list(map(_filter_name, interested_items))
+		interested_items.remove('mutalist_alad_v_nav_coordinate')
+		interested_items.remove('forma_blueprint')
+		interested_items.remove('octavia_neuroptics_blueprint')
+		results = await wilib2.fetch_all_sell_prices(session, interested_items)
+
 
 if __name__ == '__main__':
-	asyncio.run((main()))
+	asyncio.run((get_items()))
--- wilib2.py
+++ wilib2.py
@@ -44,8 +44,12 @@ async def fetch_order_sell_price(session, item_name):
 	orders = filter_order(orders)
 
 	prices = list(map(lambda x: x['platinum'], orders))
-	pos = np.argmin(prices)
-	price = prices[pos]
+	if len(prices) > 0:
+		pos = np.argmin(prices)
+		price = prices[pos]
+	else:
+		pos = None
+		price = 0
 	#pprint(orders)
 	return {'item_name' : item_name,
 			'price' : price}
@@ -76,11 +80,13 @@ async def fetch_stats(session, item_name):
 async def fetch_orders(session, item_name):
 	async with session.get(f'{_api_base_url}/{item_name}/orders') as response:
 		json = await response.json()
+		#print(json)
 		if 'orders' in json['payload']:
 			return {'item_name' : item_name,
 					'orders' : json['payload']['orders']}
 		else:
 			return None
+
 async def fetch_all_buy_prices(session, item_names):
 	results = await asyncio.gather(*[asyncio.create_task(fetch_order_buy_price(session, item_name))
 									for item_name in item_names])
@@ -108,10 +114,11 @@ async def main():
 		#all_items = await resp.json()
 		#all_items = all_items['payload']['items']['en']
 		#items = [item['url_name'] for item in all_items]
-		items = ['tigris_prime_set']
+		test_item = ['tigris_prime_set']
 
-		htmls = await fetch_all_orders(session, items)
+		htmls = await fetch_all_orders(session, test_item)
 		for item in htmls:
+			pass
 			print(item['item_name'])
 			
 			#with open('all-items-json/{}.json'.format(item['item_name']), 'w+') as f:
